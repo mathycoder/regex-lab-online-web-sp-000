2359b5bb958c4dad4c00a71dc21ff045870bb0db
Adam S <adam.sarli@gmail.com>
2019-03-11 17:40:36 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -21,5 +21,8 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/(1 )?\(?(\d{3}_\)?(\s|-)?\d{3}(\s|-)?\d{4}/) == nil
+    return false
+  end
+  return true
 end
